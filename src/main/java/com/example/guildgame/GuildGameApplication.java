6275f002a00b39a0a0fcf831be9bd9c1b7c66058
strangelavita <mohit.kumar@gmail.com>
2026-01-27 13:39:27 +0530

    test

--- src/main/java/com/example/guildgame/GuildGameApplication.java
+++ src/main/java/com/example/guildgame/GuildGameApplication.java
@@ -6,6 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class GuildGameApplication {
     public static void main(String[] args) {
+    	//Test
         SpringApplication.run(GuildGameApplication.class, args);
     }
 }
