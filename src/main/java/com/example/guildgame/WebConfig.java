c929fe73b3bff1a1b07e2cadc9c678548f59fb49
strangelavita <mohit.kumar@gmail.com>
2026-01-27 14:16:23 +0530

    CORS Config

--- src/main/java/com/example/guildgame/WebConfig.java
+++ src/main/java/com/example/guildgame/WebConfig.java
@@ -10,7 +10,10 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/api/**")
-                .allowedOrigins("http://localhost:5174" , "http://localhost:5173")
+                .allowedOriginPatterns(
+                        "http://localhost:*",
+                        "https://*.railway.app"
+                )
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                 .allowedHeaders("*");
     }
